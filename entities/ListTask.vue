3ca89f4536b5fe30600d443caa14cf00b0af2495
Artem <averin.dev@mail.ru>
2026-03-27 16:13:07 +0500

    init app

---
+++ entities/ListTask.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex">
+        <h2 class="bg-gray-200 font-bold">LIST TASK</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 class="bg-gray-200 font-bold">Welcome to the Home Page</h1>
+        <ListTask />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ListTask from '../entities/ListTask.vue'
+</script>
+
+<style scoped></style>
